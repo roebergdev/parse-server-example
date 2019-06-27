3c0e216f8f7ed4ffffc06d19afd8c15646ecb300
roebergdev <roebergdev@gmail.com>
2019-06-27 00:23:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb+srv://wynn123:wynn123@wynnwoodcluster-lrv6i.mongodb.net/test?retryWrites=true&w=majority',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'Wynnwood',
+  masterKey: process.env.MASTER_KEY || 'Wynnwood123', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://wynnwood.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
